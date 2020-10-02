aaa654976258cf29411e0613318631571bfd5bbd
Tjeu Foolen <12643433+tjeufoolen@users.noreply.github.com>
2020-10-02 16:42:15 +0200

    Added super cool and usefull ascii art

--- main.cpp
+++ main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 #include "src/curlwrapper.h"
 
+/*
+ *
+ * Searching with
+ * ,--. ,--.               ,--.
+ * |  .'   /,--.--.,--.,--.|  |
+ * |  .   ' |  .--'|  ||  ||  |
+ * |  |\   \|  |   '  ''  '|  |
+ * `--' '--'`--'    `----' `--'
+ *
+ * Made by Tjeu Foolen
+ * Commissioned by Bob Polis (Teacher Avans Hogeschool at Den Bosch)
+ * 29 October 2020
+ */
 int main()
 {
+    const std::string baseUrl = "https://www.swiftcoder.nl/cpp1/";
+
     std::unique_ptr<cUrlWrapper> cUrl{};
-    std::vector<std::string> lines = cUrl->getResponse("https://www.swiftcoder.nl/cpp1/start.txt");
+    std::vector<std::string> lines = cUrl->getResponse(baseUrl + "start.txt");
 
     // Read response line by line
     for(auto const& line: lines) {
