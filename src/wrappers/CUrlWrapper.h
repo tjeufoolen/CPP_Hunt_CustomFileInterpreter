d8345cd70bc6676ddfd7fef9cd1664d0df4bac67
Tjeu Foolen <12643433+tjeufoolen@users.noreply.github.com>
2020-10-15 14:16:16 +0200

    Update CUrlWrapper.h

--- src/wrappers/CUrlWrapper.h
+++ src/wrappers/CUrlWrapper.h
@@ -12,6 +12,13 @@ class cUrlWrapper
     public:
         cUrlWrapper();
         ~cUrlWrapper();
+
+        // prohibit copy and move (rule of five)
+        cUrlWrapper(const cUrlWrapper&) = delete;
+        cUrlWrapper& operator=(const cUrlWrapper&) = delete;
+        cUrlWrapper(cUrlWrapper&&) = delete;
+        cUrlWrapper& operator=(cUrlWrapper&&) = delete;
+
         std::vector<std::string> getResponse(const std::string& url);
     private:
         static std::size_t callback(const char* in, std::size_t size, std::size_t num, std::string* out);
